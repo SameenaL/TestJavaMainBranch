e143d920f2614a1b3dd07ea9030586ace7a71309
SameenaL <sameenalearning@gmail.com>
2025-04-27 17:20:24 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,6 @@ public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Main Branch Commit");
-        System.out.printf("Main Branch-editec Commit");
-        System.out.printf("Main Branch-edited 2nd time Commit");
 
         }
     }
-}
